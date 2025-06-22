00b79a2b90a43384e8c3a8cbdf60f477a397ee74
YoungJedauani <arthurfgalantini@gmail.com>
2025-06-22 11:47:08 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,82 @@
+
+ let listaDosNumerosSecretos = []
+ let numeroLimite = 10
+ let numeroSecreto = gerarNumeroSecreto()
+ let tentativa = 1
+ console.log (numeroSecreto)
+function exibirTextoNaTela(tag, texto){
+  let campo = document.querySelector (tag)
+  campo.innerHTML = texto
+   if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    } 
+}
+   function exibirMensagemNaTela(){
+    exibirTextoNaTela ('h1','Bem vindo ao jogo do número secreto')
+   exibirTextoNaTela ('p','Escolha um número de 1 a 10')
+   }
+   exibirMensagemNaTela()
+  
+  
+   function verificarChute(){
+   let chute = document.querySelector('input').value
+  
+    let palavraTentativa = tentativa > 1? 'tentativas' : 'tentativa'
+   let mensagemTentativa = `Você acertou o número secreto com ${tentativa} ${palavraTentativa}`
+   if (chute == numeroSecreto){
+   exibirTextoNaTela ('p', mensagemTentativa)
+   document.getElementById ('reiniciar').removeAttribute ('disabled')
+  
+  } else {
+    if (chute > numeroSecreto){
+      exibirTextoNaTela ('p', 'O número secreto é menor')
+    } else{
+      exibirTextoNaTela ('p', 'O número secreto é maior')
+    }
+    tentativa ++
+    limparCampo()
+    
+   }
+  }
+   
+ 
+ 
+
+
+ function gerarNumeroSecreto(){
+ let numeroEscolhido = parseInt(Math.random () * numeroLimite + 1)
+ let numeroSorteado = listaDosNumerosSecretos.length
+ if (numeroSorteado == numeroLimite ){
+  listaDosNumerosSecretos = []
+ }
+ if (listaDosNumerosSecretos.includes(numeroEscolhido)){
+  console.log ( listaDosNumerosSecretos)
+  return gerarNumeroSecreto()
+ } else {
+  listaDosNumerosSecretos.push ( numeroEscolhido)
+   return numeroEscolhido
+ }
+  
+
+ } 
+ 
+ 
+ function limparCampo(){
+  campo = document.querySelector('input')
+  campo.value = '';
+ }
+
+function reiniciarJogo(){
+   numeroSecreto = gerarNumeroSecreto()
+   tentativa = 1;
+   limparCampo ();
+   exibirMensagemNaTela()
+   document.getElementById('reiniciar').setAttribute('disabled', true)
+   }
+
+ 
